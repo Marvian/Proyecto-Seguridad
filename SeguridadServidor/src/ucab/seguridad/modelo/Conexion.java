0db0d47b997d4fc61777e448de375236d9ce5406
zedin1994 <94gagb@gmail.com>
2017-07-24 14:51:19 -0400

    Agregando cert y truscert a cada app

--- SeguridadServidor/src/ucab/seguridad/modelo/Conexion.java
+++ SeguridadServidor/src/ucab/seguridad/modelo/Conexion.java
@@ -5,10 +5,19 @@
  */
 package ucab.seguridad.modelo;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
 /**
  *
  * @author zedin
  */
 public class Conexion {
+    public Conexion(int puerto) throws IOException{
+        SSLServerSocketFactory serverFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        ServerSocket serverSocket = serverFactory.createServerSocket(puerto);
+        
+    }
     
 }
