5288fcbb0a48af8b3ff3eb317e35f4b1669d5840
zedin1994 <94gagb@gmail.com>
2017-07-24 20:27:34 -0400

    Agregando nuevos certificados

---
+++ SeguridadServidor/src/ucab/seguridad/modelo/Sesion.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucab.seguridad.modelo;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+
+/**
+ *
+ * @author zedin
+ */
+public class Sesion {
+    public Sesion() throws SSLPeerUnverifiedException, CertificateEncodingException, FileNotFoundException, IOException{
+        SSLSocket ss = null;
+        SSLSession sesion = ss.getSession();
+    
+
+        System.out.println("Host: " + sesion.getPeerHost());
+
+        X509Certificate certificate = (X509Certificate)sesion.getPeerCertificates()[0];
+
+        System.out.println("Propietario: " + certificate.getSubjectDN());
+
+        System.out.println("Emisor: " + certificate.getIssuerDN());
+
+        System.out.println("Numero Serie:" + certificate.getSerialNumber());
+
+        System.out.println("to string: " +certificate.toString());
+
+        byte[] buf = certificate.getEncoded();
+        FileOutputStream os = new FileOutputStream("cliente.cer");
+
+        os.write(buf);
+        os.close();
+    }
+    
+}
